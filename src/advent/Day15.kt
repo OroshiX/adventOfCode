ae3125ef802f54e3078bd3780a0fffe991b03e72
OroshiX <oroshix@users.noreply.github.com>
2020-12-15 13:58:44 +0100

    Day 15 part 1

--- src/advent/Day15.kt
+++ src/advent/Day15.kt
@@ -5,11 +5,11 @@ import java.util.*
 fun solve15(scanner: Scanner): String {
     with(scanner) {
         val numbers = nextLine().split(",").map { it.toInt() }
-        return findNumber(numbers).toString()
+        return findNumber1(numbers).toString()
     }
 }
 
-fun findNumber(numbers: List<Int>): Int {
+fun findNumber1(numbers: List<Int>): Int {
     val spokenNumbers = numbers.toMutableList()
     var currentSpoken = spokenNumbers.last()
     var turn = spokenNumbers.size
@@ -22,7 +22,7 @@ fun findNumber(numbers: List<Int>): Int {
 }
 
 fun List<Int>.differenceTwoLastIndicesOf(number: Int): Int {
-    val last = lastIndexOf(number)
+    val last = size - 1
     for (i in last - 1 downTo 0) {
         if (this[i] == number) return last - i
     }
