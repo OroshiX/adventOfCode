7da4bf7f23d1e4142830d39af0a05831bdbe58f9
OroshiX <oroshix@users.noreply.github.com>
2021-12-09 16:54:00 +0100

    Day 9 part 1

--- src/advent/Day9.kt
+++ src/advent/Day9.kt
@@ -2,9 +2,32 @@ package advent
 
 import java.util.*
 
-const val PREV = 25
 fun solve9(scanner: Scanner): String {
     with(scanner) {
-        TODO()
+        val grid = mutableListOf<List<Int>>()
+        while (hasNext()) {
+            grid += nextLine().map { Character.getNumericValue(it) }
+        }
+        return localMinimums(grid).toString()
+    }
+}
+
+private fun localMinimums(grid: List<List<Int>>): Int {
+    return grid.foldIndexed(0) { i, acc, row ->
+        acc + row.foldIndexed(0) { j, sum, number ->
+            var ok = true
+            val neighbors =
+                mutableListOf(i - 1 to j, i + 1 to j, i to j - 1, i to j + 1).filter { pair ->
+                    pair.first >= 0 && pair.second >= 0 && pair.first < grid.size && pair.second < row.size
+                }
+
+            for ((ii, jj) in neighbors) {
+                if (grid[ii][jj] <= number) {
+                    ok = false
+                    break
+                }
+            }
+            sum + (if (ok) number + 1 else 0)
+        }
     }
 }
