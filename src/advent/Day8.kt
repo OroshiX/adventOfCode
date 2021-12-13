6265a4eedbd0d77e8c42218a34f545f35b38c033
OroshiX <oroshix@users.noreply.github.com>
2021-12-13 13:44:45 +0100

    Day 8 part 1

--- src/advent/Day8.kt
+++ src/advent/Day8.kt
@@ -3,7 +3,24 @@ package advent
 import java.util.*
 
 fun solve8(scanner: Scanner): String {
+    val inputOutput = mutableListOf<Pair<List<String>, List<String>>>()
     with(scanner) {
-        TODO()
+        while (hasNext()) {
+            val (input, output) = nextLine().split("|")
+            inputOutput += input.trim().split(" ") to output.trim().split(" ")
+        }
+        return part1(inputOutput).toString()
     }
 }
+
+private fun part1(inputOutput: List<Pair<List<String>, List<String>>>): Int {
+    return inputOutput.fold(0) { acc, pair ->
+        acc + pair.second.count {
+            when (it.length) {
+                2, 4, 3, 7 -> true
+                else -> false
+            }
+        }
+    }
+}
+
