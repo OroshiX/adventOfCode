3f10095d0d8137e1ca5d515c6d9d450ae3eeaee4
OroshiX <oroshix@users.noreply.github.com>
2022-12-04 17:08:46 +0100

    Day 3 part 1

--- src/advent/Day3.kt
+++ src/advent/Day3.kt
@@ -2,16 +2,39 @@ package advent
 
 import java.util.*
 
-class Day3 : DayPuzzle<List<Int>>(3, true) {
-    override fun parse(scanner: Scanner): List<Int> {
-        TODO("Not yet implemented")
+class Day3 : DayPuzzle<List<String>>(3, false) {
+    override fun parse(scanner: Scanner): List<String> {
+        with(scanner) {
+            val input = mutableListOf<String>()
+            while (hasNextLine()) {
+                input += nextLine()
+            }
+            return input
+        }
     }
 
-    override fun solve1(input: List<Int>): String {
-        TODO("Not yet implemented")
+
+    override fun solve1(input: List<String>): String {
+        return input.sumOf { rucksack ->
+            rucksack.commonItem()?.priority() ?: 0
+        }.toString()
     }
 
-    override fun solve2(input: List<Int>): String {
+    override fun solve2(input: List<String>): String {
         TODO("Not yet implemented")
+    }
+}
+
+private fun String.commonItem(): Char? {
+    return substring(0 until length / 2).toSet()
+        .intersect(substring(length / 2 until length).toSet())
+        .firstOrNull()
+}
+
+private fun Char.priority(): Int {
+    return if (this.isLowerCase()) {
+        this - 'a' + 1
+    } else {
+        this - 'A' + 27
     }
 }
