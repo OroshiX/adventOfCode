d53b61732ab79edb6597f2f04c7018d882b8de36
OroshiX <oroshix@users.noreply.github.com>
2024-12-05 20:01:07 +0100

    Day 15 part 1

--- src/main/kotlin/advent/days/Day15.kt
+++ src/main/kotlin/advent/days/Day15.kt
@@ -3,16 +3,24 @@ package advent.days
 import advent.DayPuzzle
 import java.util.*
 
-class Day15 : DayPuzzle<List<Int>>() {
-    override fun parse(scanner: Scanner): List<Int> {
-        TODO("Not yet implemented")
+class Day15 : DayPuzzle<List<String>>() {
+    override fun parse(scanner: Scanner): List<String> {
+        return scanner.nextLine().split(",")
     }
 
-    override fun solve1(input: List<Int>): String {
-        TODO("Not yet implemented")
+    override fun solve1(input: List<String>): String {
+        return input.sumOf { hash(it) }.toString()
     }
 
-    override fun solve2(input: List<Int>): String {
+    private fun hash(sequence: String): Int {
+        var hash = 0
+        for (c in sequence) {
+            hash = ((hash + c.code) * 17) % 256
+        }
+        return hash
+    }
+
+    override fun solve2(input: List<String>): String {
         TODO("Not yet implemented")
     }
 
