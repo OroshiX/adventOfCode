b9f937afb4f81d2905cb24699a66a4d782cb5ff0
OroshiX <oroshix@users.noreply.github.com>
2024-12-05 21:14:05 +0100

    Day 1 part 1

--- src/main/kotlin/advent/days/Day1.kt
+++ src/main/kotlin/advent/days/Day1.kt
@@ -2,17 +2,28 @@ package advent.days
 
 import advent.DayPuzzle
 import java.util.*
+import kotlin.math.abs
 
-class Day1 : DayPuzzle<String>() {
-    override fun parse(scanner: Scanner): String {
-        TODO()
+class Day1 : DayPuzzle<Pair<List<Int>, List<Int>>>() {
+
+    override fun parse(scanner: Scanner): Pair<List<Int>, List<Int>> {
+        val list1 = mutableListOf<Int>()
+        val list2 = mutableListOf<Int>()
+        while (scanner.hasNextInt()) {
+            list1.add(scanner.nextInt())
+            list2.add(scanner.nextInt())
+        }
+        return list1 to list2
     }
 
-    override fun solve1(input: String): String {
-        TODO()
+    override fun solve1(input: Pair<List<Int>, List<Int>>): String {
+        val sorted = input.first.sorted()
+        val sorted2 = input.second.sorted()
+        val sumDiffs = (sorted zip sorted2).sumOf { abs(it.second - it.first) }
+        return sumDiffs.toString()
     }
 
-    override fun solve2(input: String): String {
+    override fun solve2(input: Pair<List<Int>, List<Int>>): String {
         TODO()
     }
 }
