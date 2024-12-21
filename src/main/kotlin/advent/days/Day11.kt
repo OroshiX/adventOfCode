3ed3f6dfccfbe7766e823a8da519af9615f9edcc
OroshiX <oroshix@users.noreply.github.com>
2024-12-21 20:29:12 +0100

    Day 11 part 1 working, part 2 is OOM

--- src/main/kotlin/advent/days/Day11.kt
+++ src/main/kotlin/advent/days/Day11.kt
@@ -1,18 +1,56 @@
 package advent.days
 
 import advent.DayPuzzle
+import java.math.BigInteger
 import java.util.*
+import kotlin.math.log10
+import kotlin.math.pow
 
-class Day11 : DayPuzzle<String>() {
-    override fun parse(scanner: Scanner): String {
-        TODO()
+class Day11 : DayPuzzle<List<BigInteger>>() {
+    override fun parse(scanner: Scanner): List<BigInteger> {
+        return scanner.nextLine().split(" ").map { BigInteger(it) }
     }
 
-    override fun solve1(input: String): String {
-        TODO()
+    override fun solve1(input: List<BigInteger>): String {
+        var current = input
+        repeat(25) {
+            current = current.nextStep()
+        }
+        return current.size.toString()
     }
 
-    override fun solve2(input: String): String {
-        TODO()
+    override fun solve2(input: List<BigInteger>): String {
+        var current = input
+        repeat(75) {
+            current = current.nextStep()
+        }
+        return current.size.toString()
     }
 }
+
+private val MULTIPLIER = BigInteger.valueOf(2024)
+
+private fun List<BigInteger>.nextStep(): List<BigInteger> {
+    val result = mutableListOf<BigInteger>()
+    for (item in this) {
+        if (item == BigInteger.ZERO) {
+            result.add(BigInteger.ONE)
+        } else {
+            val digits = item.numberOfDigits()
+            if (digits % 2 == 0) {
+                // get first it digits of item
+                (digits / 2).let {
+                    val pow = BigInteger.valueOf(10.0.pow(it.toDouble()).toLong())
+                    result.add(item / pow)
+                    result.add(item % pow)
+                }
+            } else result.add(item * MULTIPLIER)
+        }
+    }
+    return result
+}
+
+/**
+ * Precondition: this > 0
+ */
+fun BigInteger.numberOfDigits(): Int = (log10(this.toFloat()) + 1).toInt()
