42666e9c7fac210dd2fb913764a6fa86137ba0b9
OroshiX <oroshix@users.noreply.github.com>
2025-12-09 20:37:53 +0100

    Day 3 part 1

--- src/main/kotlin/advent/days/Day3.kt
+++ src/main/kotlin/advent/days/Day3.kt
@@ -3,16 +3,49 @@ package advent.days
 import advent.DayPuzzle
 import java.util.*
 
-class Day3 : DayPuzzle<String>() {
-    override fun parse(scanner: Scanner): String {
-        TODO()
+class Day3 : DayPuzzle<List<String>>() {
+    override fun parse(scanner: Scanner): List<String> {
+        val lines = mutableListOf<String>()
+        while (scanner.hasNextLine()) {
+            val line = scanner.nextLine().trim()
+            if (line.isNotEmpty()) {
+                lines.add(line)
+            }
+        }
+        return lines
     }
 
-    override fun solve1(input: String): String {
-        TODO()
+    override fun solve1(input: List<String>): String {
+        var sum = 0
+        for (line in input) {
+            sum += line.biggestNumber()
+        }
+        return sum.toString()
     }
 
-    override fun solve2(input: String): String {
+    override fun solve2(input: List<String>): String {
         TODO()
     }
+}
+
+private fun String.biggestNumber(): Int {
+    var maxFirst = 0
+    var currentFirst = 0
+    var indexOfMax = -1
+    for (i in 0 until this.length - 1) {
+        currentFirst = this[i].digitToInt()
+        if (currentFirst > maxFirst) {
+            maxFirst = currentFirst
+            indexOfMax = i
+        }
+    }
+    var maxSecond = 0
+    var currentSecond = 0
+    for (i in indexOfMax + 1 until this.length) {
+        currentSecond = this[i].digitToInt()
+        if (currentSecond > maxSecond) {
+            maxSecond = currentSecond
+        }
+    }
+    return maxFirst * 10 + maxSecond
 }
