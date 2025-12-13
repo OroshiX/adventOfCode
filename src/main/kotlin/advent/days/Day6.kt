c3345f2610123fe4f6fa317869ad736ce96d6825
OroshiX <oroshix@users.noreply.github.com>
2025-12-13 14:41:30 +0100

    Day 6 part 1

--- src/main/kotlin/advent/days/Day6.kt
+++ src/main/kotlin/advent/days/Day6.kt
@@ -3,16 +3,75 @@ package advent.days
 import advent.DayPuzzle
 import java.util.*
 
-class Day6 : DayPuzzle<String>() {
-    override fun parse(scanner: Scanner): String {
-        TODO()
+class Day6 : DayPuzzle<List<MathProblem>>() {
+    override fun parse(scanner: Scanner): List<MathProblem> {
+        val lines = mutableListOf<String>()
+        while (scanner.hasNext()) {
+            val line = scanner.nextLine()
+            lines.add(line)
+        }
+        val size = lines.first().split(" ").filter { it.isNotBlank() }.size
+        val numbers = List(size) { List(lines.size - 1) { -1L }.toMutableList() }.toMutableList()
+        val operators = List(size) { Operation.Add }.toMutableList()
+
+        for (i in lines.indices) {
+            val line = lines[i]
+            if (i == lines.lastIndex) {
+                // operator
+                line.split(" ").filterNot { it.isBlank() }.forEachIndexed { j, operation ->
+                    operators[j] = Operation.from(operation)
+                }
+            } else {
+                // numbers
+                line.split(" ").filterNot { it.isBlank() }.forEachIndexed { j, number ->
+                    numbers[j][i] = number.toLong()
+                }
+            }
+        }
+        return List(size) {
+            MathProblem(numbers = numbers[it], operation = operators[it])
+        }
     }
 
-    override fun solve1(input: String): String {
-        TODO()
+    override fun solve1(input: List<MathProblem>): String {
+        var sum = 0L
+        for (mathProblem in input) {
+            sum += mathProblem.solve()
+        }
+        return sum.toString()
     }
 
-    override fun solve2(input: String): String {
+    override fun solve2(input: List<MathProblem>): String {
         TODO()
+    }
+}
+
+data class MathProblem(val numbers: List<Long>, val operation: Operation) {
+    fun solve(): Long {
+        var res = numbers.first()
+        for (index in 1 until numbers.size) {
+            val number = numbers[index]
+            when (operation) {
+                Operation.Add -> {
+                    res += number
+                }
+
+                Operation.Multiply -> {
+                    res *= number
+                }
+            }
+        }
+        return res
+    }
+}
+
+enum class Operation(val char: String) {
+    Add("+"),
+    Multiply("*");
+
+    companion object {
+        fun from(char: String): Operation {
+            return entries.first { it.char == char }
+        }
     }
 }
