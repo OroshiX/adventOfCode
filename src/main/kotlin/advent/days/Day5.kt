2d81741750c19bd8c5213f3475ee5c52455d3216
OroshiX <oroshix@users.noreply.github.com>
2025-12-13 13:49:12 +0100

    Day 5 part 2

--- src/main/kotlin/advent/days/Day5.kt
+++ src/main/kotlin/advent/days/Day5.kt
@@ -2,6 +2,8 @@ package advent.days
 
 import advent.DayPuzzle
 import java.util.*
+import kotlin.math.max
+import kotlin.math.sign
 
 class Day5 : DayPuzzle<IngredientList>() {
     override fun parse(scanner: Scanner): IngredientList {
@@ -34,7 +36,13 @@ class Day5 : DayPuzzle<IngredientList>() {
     }
 
     override fun solve2(input: IngredientList): String {
-        TODO()
+        val sortRanges = input.ranges.sortRanges()
+        val separatedList = sortRanges.toSeparateRanges()
+        var sum = 0L
+        for (separated in separatedList) {
+            sum += separated.last - separated.first + 1
+        }
+        return sum.toString()
     }
 }
 
@@ -45,4 +53,69 @@ data class IngredientList(
     fun isFresh(ingredient: Long): Boolean {
         return ranges.any { it.contains(ingredient) }
     }
+
+}
+
+private fun List<LongRange>.sortRanges(): List<LongRange> {
+    return sortedWith { o1, o2 ->
+        when {
+            o1.first == o2.first -> (o2.last - o1.last).sign
+            else -> (o1.first - o2.first).sign
+        }
+    }
+}
+
+fun areSeparated(range1: LongRange, range2: LongRange): Boolean {
+    return when {
+        range1.first > range2.first -> throw IllegalStateException("Badly ordered ranges ${range1.prettyString()} and ${range2.prettyString()}")
+        range1.first > range2.last -> throw IllegalStateException("Badly ordered ranges ${range1.prettyString()} and ${range2.prettyString()}")
+        range2.first <= range1.last -> false
+        range2.first > range1.last -> true
+        else -> throw IllegalStateException("Should not happen")
+    }
+}
+
+private fun List<LongRange>.toSeparateRanges(): List<LongRange> {
+    val ranges = mutableListOf<LongRange>()
+    var i = 0
+    var range1 = this[0]
+    var lastAddedIndex = 0
+    ranges.add(range1)
+    outer@ while (i < this.size) {
+        for (j in i + 1 until this.size) {
+            val range2 = this[j]
+
+            if (areSeparated(range1, range2)) {
+                ranges.add(range2)
+                lastAddedIndex++
+                i = j
+                range1 = range2
+                continue@outer
+            }
+            // ranges are overlapping
+            val newRange = mergeRanges(range1, range2)
+            ranges[lastAddedIndex] = newRange
+            range1 = newRange
+            i = j
+        }
+        i++
+        if (i >= size) break@outer
+        range1 = this[i]
+    }
+    return ranges
+}
+
+private fun mergeRanges(range1: LongRange, range2: LongRange): LongRange {
+    if (range1.last < range2.first || range2.last < range1.first) {
+        throw IllegalStateException("Ranges are separated ${range1.prettyString()} and ${range2.prettyString()}")
+    }
+    return range1.first..max(range1.last, range2.last)
+}
+
+fun LongRange.prettyString(): String {
+    return first.prettyPrint() + ".." + last.prettyPrint()
+}
+
+fun Long.prettyPrint(): String {
+    return toString().reversed().chunked(3).joinToString("'").reversed()
 }
---
+++ src/test/kotlin/advent/days/Day5Test.kt
@@ -0,0 +1,35 @@
+package advent.days
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class Day5Test {
+    @Test
+    fun areSeparatedTest() {
+        val tests = listOf(
+            RangeTestCase(1L..3L, 2L..4L, false),
+            RangeTestCase(1L..3L, 5L..7L, true),
+            RangeTestCase(2L..5L, 1L..2L, false, expectError = true),
+            RangeTestCase(2L..0L, 5L..0L, false, expectError = true),
+
+        )
+        for (test in tests) {
+            if (test.expectError) {
+                shouldThrow<IllegalStateException> {
+                    areSeparated(test.range1, test.range2)
+                }
+            } else {
+                val result = areSeparated(test.range1, test.range2)
+                result shouldBe test.separated
+            }
+        }
+    }
+}
+
+private data class RangeTestCase(
+    val range1: LongRange,
+    val range2: LongRange,
+    val separated: Boolean,
+    val expectError: Boolean = false
+)
