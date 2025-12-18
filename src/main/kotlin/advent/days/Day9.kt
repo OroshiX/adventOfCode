637b2639eddff5de8f8a3eba11a2ccc9fcf222de
OroshiX <oroshix@users.noreply.github.com>
2025-12-18 08:23:35 +0100

    Day 9 part 1

--- src/main/kotlin/advent/days/Day9.kt
+++ src/main/kotlin/advent/days/Day9.kt
@@ -2,17 +2,40 @@ package advent.days
 
 import advent.DayPuzzle
 import java.util.*
+import kotlin.math.max
+import kotlin.math.min
 
-class Day9 : DayPuzzle<String>() {
-    override fun parse(scanner: Scanner): String {
-        TODO()
+class Day9 : DayPuzzle<List<RedTile>>() {
+    override fun parse(scanner: Scanner): List<RedTile> {
+        val redTiles = mutableListOf<RedTile>()
+        while (scanner.hasNext()) {
+            val line = scanner.next()
+            val (x, y) = line.split(",").map { it.toLong() }
+            redTiles.add(RedTile(x, y))
+        }
+        return redTiles
     }
 
-    override fun solve1(input: String): String {
-        TODO()
+    override fun solve1(input: List<RedTile>): String {
+        var areaMax = 0L
+        for (u in input.indices) {
+            for (v in u + 1 until input.size) {
+                val tile1 = input[u]
+                val tile2 = input[v]
+                val xDistance = max(tile2.x, tile1.x) - min(tile1.x, tile2.x) + 1
+                val yDistance = max(tile2.y, tile1.y) - min(tile1.y, tile2.y) + 1
+                val area = xDistance * yDistance
+                if (area > areaMax) {
+                    areaMax = area
+                }
+            }
+        }
+        return areaMax.toString()
     }
 
-    override fun solve2(input: String): String {
+    override fun solve2(input: List<RedTile>): String {
         TODO()
     }
 }
+
+data class RedTile(val x: Long, val y: Long)
